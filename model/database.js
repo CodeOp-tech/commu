c1b4f048539a78c94d52981e38eeb88f686889b5
Alessandra Sampalean <asampalean@icloud.com>
2020-02-28 11:36:57 +0100

    added thedatabase

--- model/database.js
+++ model/database.js
@@ -10,7 +10,7 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "todos",
+  database: DB_NAME || "commu",
   multipleStatements: true
 });
 
@@ -18,15 +18,73 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql = "DROP TABLE if exists items; CREATE TABLE items(id INT NOT NULL AUTO_INCREMENT, text VARCHAR(40) not null, complete BOOLEAN, PRIMARY KEY (id));";
-  con.query(sql, function (err, result) {
+  let sql = `DROP TABLE if exists users; CREATE TABLE users(  id INT NOT NULL AUTO_INCREMENT,
+    full_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    area_id INT(255) NOT NULL,
+    img varchar(255) NOT NULL,
+    skills varchar(255) NOT NULL,
+    about varchar(255) NOT NULL,
+    PRIMARY KEY (id));`;
+
+  con.query(sql, function(err, result) {
     if (err) throw err;
-    console.log("Table creation `items` was successful!");
+    console.log("Table creation `users` was successful!");
+
+    console.log("Closing...");
+  });
+
+  sql = `DROP TABLE if exists jobs; CREATE TABLE jobs(  id INT NOT NULL AUTO_INCREMENT,
+user_id INT NOT NULL,
+ description varchar(255) NOT NULL,
+price INT(255) NOT NULL,
+  date_time varchar(255) NOT NULL,
+ PRIMARY KEY (id) );`;
+
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Table creation `jobs` was successful!");
+
+    console.log("Closing...");
+  });
+
+  sql = `DROP TABLE if exists areas; CREATE TABLE areas(  id INT NOT NULL AUTO_INCREMENT,
+hood INT NOT NULL,
+PRIMARY KEY (id));`;
+
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Table creation `areas` was successful!");
+
+    console.log("Closing...");
+  });
+
+  sql = `DROP TABLE if exists favorites; CREATE TABLE favorites( id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+ profile_id INT NOT NULL,
+  PRIMARY KEY (id));`;
+
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Table creation `favorites` was successful!");
+
+    console.log("Closing...");
+  });
+
+  sql = `DROP TABLE if exists ratings; CREATE TABLE ratings(
+    id INT NOT NULL AUTO_INCREMENT,
+  rating INT NOT NULL,
+  user_id INT NOT NULL,
+    PRIMARY KEY (id)
+   );`;
+
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Table creation `ratings` was successful!");
 
     console.log("Closing...");
   });
 
   con.end();
 });
-
-
