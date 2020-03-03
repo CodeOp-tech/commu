77c44ebe5996a97eb8e0764b87096bd39944f028
Alessandra Sampalean <asampalean@icloud.com>
2020-03-03 17:57:51 +0100

    Auth login system

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,11 +1,58 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  attemptLogin = () => {
+    axios("/users/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      data: { email: this.state.email, password: this.state.password }
+    })
+      .then(results => console.log(results))
+      .catch(err => console.log(err));
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    this.attemptLogin();
+  };
+
   render() {
     return (
       <div>
-        <h1>Look through your community!</h1>
-        <button>Login</button>
+        <h1>Login page</h1>
+        <form onSubmit={this.handleSubmit} method="POST">
+          <input
+            name="email"
+            type="text"
+            onChange={this.handleInput}
+            value={this.state.email}
+            className="form-control"
+          />
+          <input
+            name="password"
+            type="password"
+            onChange={this.handleInput}
+            value={this.state.password}
+            className="form-control"
+          />
+          <button onClick={this.attemptLogin}>Login</button>
+        </form>
+        {/* <Link to="/home" >Login</Link> */}
       </div>
     );
   }
