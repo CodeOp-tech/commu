aa768498b13ff514faad367fea673a1cf6c6ec17
tothviktoria <viktoriatoth.urban.couture@gmail.com>
2020-03-04 15:15:57 +0100

    components

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+
 export default class Community extends Component {
   render() {
     return (
       <div class="container-fluid">
         <div>
           <Link class="text-decoration-none text-dark" to="/community">
-            <div
+            <button
               type="button"
               class="btn btn-light shadow p-3 mb-5 bg-white rounded"
             >
@@ -15,8 +16,29 @@ export default class Community extends Component {
                 class="d-inline-block align-top"
                 alt=""
               />
+            </button>
+            <div class="row">
+              <div class="col-md-4 col-md-offset-4">
+                Community members:
+                <div class="card text-center">
+                  <img
+                    src="https://i.imgur.com/eQXVxYR.png"
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">Member</h5>
+                    <p class="card-text">Member name</p>
+                    <button
+                      type="button"
+                      class="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                    >
+                      Profile
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="row">Community members:</div>
           </Link>
         </div>
       </div>
