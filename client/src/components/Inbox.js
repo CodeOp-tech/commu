99c606fed5a29ddf156aba8592903857d83344b1
tothviktoria <viktoriatoth.urban.couture@gmail.com>
2020-03-05 18:21:52 +0100

    inbox favorites details doing

--- client/src/components/Inbox.js
+++ client/src/components/Inbox.js
@@ -4,11 +4,83 @@ import { Link } from "react-router-dom";
 export default class Inbox extends Component {
   render() {
     return (
-      <div class="container-fluid">
-        <div>
-          <Link class="text-decoration-none text-dark" to="/inbox"></Link>
-          INBOX
-        </div>
+      <div className="container">
+        <nav id="sidebar">
+          <div className="sidebar-header">
+            <h3>
+              <Link className="text-decoration-none text-dark" to="/inbox">
+                <button
+                  type="button"
+                  class="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                >
+                  POSTBOX
+                </button>
+              </Link>
+            </h3>
+          </div>
+
+          <ul className="list-unstyled components">
+            <p>
+              <Link className="text-decoration-none text-dark" to="/inbox">
+                <button
+                  type="button"
+                  className="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                >
+                  MESSAGES
+                </button>
+              </Link>
+            </p>
+            <div className="card shadow p-3 mb-5 bg-white rounded">
+              <div className="card-body">
+                <li className="active">
+                  <a
+                    href="#homeSubmenu"
+                    data-toggle="collapse"
+                    aria-expanded="false"
+                    className="dropdown-toggle"
+                  >
+                    <div
+                      type="button"
+                      className="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                    >
+                      <img
+                        src="https://i.imgur.com/DPtqACQ.png"
+                        className="d-inline-block align-top"
+                        alt=""
+                      />
+                    </div>
+                  </a>
+                  <ul className="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                      <div
+                        type="button"
+                        className="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                      >
+                        Received
+                      </div>
+                    </li>
+                    <li>
+                      <div
+                        type="button"
+                        className="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                      >
+                        Sent
+                      </div>
+                    </li>
+                    <li>
+                      <div
+                        type="button"
+                        className="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                      >
+                        Write
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </div>
+            </div>
+          </ul>
+        </nav>
       </div>
     );
   }
