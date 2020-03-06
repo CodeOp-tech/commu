f32b320a9df55794713be8cb0210db2e08922563
Daena-Michelle <58523120+Daena-Michelle@users.noreply.github.com>
2020-03-06 15:03:09 +0100

    community full-stack connection

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -1,32 +1,61 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+//
 export default class Community extends Component {
+//
+    constructor(props) {
+        super(props);
+        this.state = {
+              // full_name: "",
+              // area_id: 0,
+              // img: "",
+              // skills: [],
+              // about: "",
+              users: []
+        }
+    }
+//
+    componentDidMount = () => {
+        fetch(`/users`)
+        // fetch(`/:area_id/users`) should work when we got the token that states the viewer's area
+        .then(response => response.json())
+        .then(response => {
+            this.setState({ users: response });
+        });
+    };
+//
   render() {
     return (
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-4 col-md-offset-4">
-            Community members:
-            <div class="card text-center">
-              <img
-                src="https://i.imgur.com/eQXVxYR.png"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Member</h5>
-                <p class="card-text">Member name</p>
-                <button
-                  type="button"
-                  class="btn btn-light shadow p-3 mb-5 bg-white rounded"
-                >
-                  Profile
-                </button>
-              </div>
+      <div class="container-fluid p-5">
+        <div class="row pb-5">
+            <h1>Community members:</h1>
+            <div class="row py-5">
+            {this.state.users.map((user, i) => {
+              return (
+                <div key={i} class="col-lg-2 col-md-3 col-sm-4 col-6 d-flex align-items-stretch pb-5">
+                <div class="card text-center shadow">
+                  <img
+                    src={user.img}
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">{user.full_name}</h5>
+                    <p class="card-text"><span>Skills: </span>{user.skills}</p>
+                    <button
+                      type="button"
+                      class="btn btn-light shadow p-3 mb-5 bg-white rounded mt-auto"
+                    >
+                      Profile
+                    </button>
+                    {/* we need a modal window that shows the profile here */}
+                  </div>
+                </div>
+                </div>
+              )
+            })}
             </div>
-          </div>
-        </div>
+            </div>
       </div>
     );
   }
