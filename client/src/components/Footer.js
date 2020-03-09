6d980eb2c513691444ca53bdd7f6155e5dd05038
tothviktoria <viktoriatoth.urban.couture@gmail.com>
2020-03-09 18:56:38 +0100

    styling start

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./Footer.css";
 
 export default class Footer extends Component {
   render() {
     return (
-      <div class="container-fluid">
-        <ul>
-          <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-              <div>
-                <Link class="text-decoration-none text-dark" to="/home">
+      <div className="container py-4 text-center">
+        <ul className="backgroundStyle">
+          <div className="row text-center">
+            <div className="footerDesign mx-auto">
+              <Link className="text-decoration-none text-dark" to="/home">
+                <div
+                  type="button"
+                  className="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                >
                   About
-                </Link>
-              </div>
+                </div>
+              </Link>
             </div>
 
-            <div class="col-md-4 col-md-offset-4">
-              <Link class="text-decoration-none text-dark" to="/home">
-                Guidelines
+            <div className="footerDesign mx-auto">
+              <Link className="text-decoration-none text-dark" to="/home">
+                <div
+                  type="button"
+                  className="btn btn-light shadow p-3 mb-5 bg-white rounded"
+                >
+                  Guidelines
+                </div>
               </Link>
             </div>
           </div>
