30213daa1c344f5a225e8152211dec4b2c7bd67b
tothviktoria <viktoriatoth.urban.couture@gmail.com>
2020-03-10 11:30:55 +0100

    withoutmodal

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -12,11 +12,6 @@ export default class Community extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // full_name: "",
-      // area_id: 0,
-      // img: "",
-      // skills: [],
-      // about: "",
       users: []
     };
   }
@@ -58,8 +53,6 @@ export default class Community extends Component {
                       <button
                         type="button"
                         class="btn btn-light shadow p-3 mb-5 bg-white rounded mt-auto"
-                        data-toggle="modal"
-                        data-target="#exampleModal"
                       >
                         Profile
                       </button>
@@ -71,51 +64,6 @@ export default class Community extends Component {
             })}
           </div>
         </div>
-        <div
-          class="modal fade"
-          id="exampleModal"
-          tabindex="-1"
-          role="dialog"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">
-                  Member
-                </h5>
-                <button
-                  type="button"
-                  class="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <Community />
-              </div>
-              <div class="modal-footer">
-                <button
-                  type="button"
-                  class="btn btn-secondary"
-                  data-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button type="button" class="btn btn-primary">
-                  Save changes
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <footer>
-          <Footer />
-        </footer>
       </div>
     );
   }
