59de28eb26dd5a305997c60a3adac72c8d873db7
Daena-Michelle <58523120+Daena-Michelle@users.noreply.github.com>
2020-03-13 17:14:51 +0100

    Bugfixing

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -34,99 +34,31 @@ export default class Community extends Component {
   //
   render() {
     return (
-      <div class="container fluid p-5">
-        <div>
+      <div>
+        <div class="container p-4">
           <h3>
             <Header />
           </h3>
           <div className="heroCommunity">
             <p></p>
           </div>
-          <div class="row">
+          <div class="row py-5">
             {this.state.users.map((user, i) => {
               return (
                 <div
                   key={i}
-                  class="col-lg-3 col-md-4 col-6 d-flex
-                  align-items-stretch pt-4 mx-auto text-center w-100 pb-5"
+                  class="col-lg-4 col-md-3 col-sm-4 col-6 d-flex align-items-stretch pb-5"
                 >
                   <div class="card text-center shadow">
-                    <img src={user.img} class="card-img-top img-fluid w-100" alt="..." />
+                    <img src={user.img} class="card-img-top" alt="..." />
                     <div class="card-body d-flex flex-column">
                       <h5 class="card-title">{user.full_name}</h5>
                       <p class="card-text">
                         <span>Skills: </span>
                         {user.skills}
                       </p>
-                      </div>
                       <div class="card-footer p-3">
-<<<<<<< HEAD
                         <div class="row">
-=======
-                      <div class="row">
-                        <div class="col-12 col-lg-6">
-                        <button type="button" class="btn btn-light shadow bg-white rounded align-self-end">
-                              Message
-                            {/* we need to connect the Pusher chatApp to this button*/}
-                            </button>
-                        </div>
-                        <div class="col-12 col-lg-6">
-                        <button
-                            type="button"
-                            class="btn btn-secondary shadow rounded align-self-end"
-                            data-toggle="modal"
-                            data-target="#exampleModalCenter"
-                            onClick={() => this.setCurrentUser(user)}
-                          >
-                            Profile
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-
-            </div>
-            </div>
-            )})}
-
-                  {/* <!-- Modal --> */}
-                  <div
-                    class="modal fade"
-                    id="exampleModalCenter"
-                    tabindex="-1"
-                    role="dialog"
-                    aria-labelledby="exampleModalCenterTitle"
-                    aria-hidden="true"
-                  >
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalCenterTitle">
-                            {this.state.user.full_name}
-                          </h5>
-                          <button
-                            type="button"
-                            class="close"
-                            data-dismiss="modal"
-                            aria-label="Close"
-                          >
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>
-                        <div class="modal-body">
-                        <img
-                          className="img-responsive modal-portrait"
-                          src={this.state.user.img}
-                          alt="portrait"
-                        />
-                      <div className="list-group-item">
-                        {this.state.user.about}
-                      </div>
-                      <div className="list-group-item">
-                      <span>Skills: </span>{this.state.user.skills}</div>
-                    </div>
-                        <div class="modal-footer">
-                          <div class="row">
->>>>>>> Bootstrap + icon
                           <div class="col-6">
                             <button
                               type="button"
